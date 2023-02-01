cf9d9a1b9d3c332802e1b225501d36a471f14762
GustavoRizzo <grizzo.albu@gmail.com>
2023-02-01 17:49:31 -0300

    Create a model for Polls App

--- studyingDjango/settings.py
+++ studyingDjango/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
-    "debug_toolbar",
+    'debug_toolbar',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
