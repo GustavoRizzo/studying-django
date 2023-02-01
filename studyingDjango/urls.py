73ffeea1ec6faf68865bda8e756fbb71e39f8d38
GustavoRizzo <grizzo.albu@gmail.com>
2023-01-31 21:07:50 -0300

    Create a View for polls

--- studyingDjango/urls.py
+++ studyingDjango/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
+    path('polls', include('polls.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
