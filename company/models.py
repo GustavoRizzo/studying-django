e3362dc3e9791e97c8d3793916a081d86a5990b5
GustavoRizzo <grizzo.albu@gmail.com>
2023-02-03 15:44:21 -0300

    add url_img to Compani Member model

--- company/models.py
+++ company/models.py
@@ -14,6 +14,7 @@ class Member(models.Model):
     fk_company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     hiring_date = models.DateField(default= date.today)
+    url_img = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
