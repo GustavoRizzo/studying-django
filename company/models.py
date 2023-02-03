cbbfddd1c056fc5da6a7c269993620ffd196b4a3
GustavoRizzo <grizzo.albu@gmail.com>
2023-02-03 13:54:12 -0300

    add Style to Company Index

--- company/models.py
+++ company/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from datetime import date
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
-    founding_date = models.DateTimeField()
+    url_img = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
@@ -12,7 +13,7 @@ class Company(models.Model):
 class Member(models.Model):
     fk_company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    hiring_date = models.DateTimeField(default= timezone.now)
+    hiring_date = models.DateField(default= date.today)
 
     def __str__(self):
         return self.name
