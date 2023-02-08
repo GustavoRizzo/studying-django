51255bc0e8403fac6f7c42c38b6f92c7578fbfcc
GustavoRizzo <grizzo.albu@gmail.com>
2023-02-07 22:17:10 -0300

    change Oferta models

--- ofertas/models.py
+++ ofertas/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Oferta(models.Model):
     code = models.CharField(max_length=10)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=False)
     url_img = models.CharField(max_length=500)
+    url_img2 = models.CharField(max_length=500)
     supplier_link = models.CharField(max_length=500)
+    supplier_link2 = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
